a993a50cb2d390c53b7e11f97617cbe49cd934f9
Alvaro Yañez <overflow075@gmail.com>
2026-05-11 22:21:59 -0400

    Avance de la solucion 1

--- solucion1.cpp
+++ solucion1.cpp
@@ -3,4 +3,33 @@
 using namespace std;
 #include <cstdlib>
 #include <vector>
+#include <fstream>
+#include <cstring>
+
+vector <unsigned char*> Palabras;
+
+int indice[28];
+
+
+void ordenar_lexico( vector<unsigned char*>& Palabras,unsigned char*, int indice);
+
+void cargarDiccionario(const string& nombreArchivo, vector<unsigned char*>& Palabras,int indice) {
+    ifstream archivo(nombreArchivo);
+    string linea;
+     while (getline(archivo, linea)) {
+        if (linea.empty()) continue;
+
+        // 1. Reservar memoria exacta para la palabra (incluyendo el terminador \0)
+        unsigned char* nuevaClave = new unsigned char[linea.length() + 1];
+
+        // 2. Copiar el contenido de la linea al puntero uchar*
+        strcpy(reinterpret_cast<char*>(nuevaClave), linea.c_str());
+
+
+
+        ordenar_lexico(Palabras,nuevaClave,indice);
+
+
+     }}
+
 
