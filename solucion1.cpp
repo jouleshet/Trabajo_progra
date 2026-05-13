aedf969315639b4b38b5aef48a8f6f062784868c
nylon <danilo.arce.castro@gmail.com>
2026-05-12 20:04:28 -0400

    avance solucion 1

--- solucion1.cpp
+++ solucion1.cpp
@@ -1,3 +1,12 @@
+/*QUE HACER
+   CREAR ESTRUCTURA QUE GUARDA PALABRAS EN UN ARREGLO O VECTOR, EL CUAL TIENE QUE ESTAR SIEMPRE ORDENADO
+   EL VECTOR TIENE CAPACIDAD MAXIMA, SI SE LLENA CREAR OTRO, QUE TENGA MAYOR CAPACIDAD, PERO QUE EL AUMENTO SEA
+   DECENTE, OSEA, NO AUMENTARLO EN 1 PQ SINO CADA PALABRA HARIA OTRO MAS esto es el OVERHEAD
+
+   usar un INDICE que guarde las posiciones de las letras, asi al buscar un elemento solo uso el valor del indice de la 1ra letra y la 
+   letra siguiente, esto es la BUSQUEDA BINARIA ACOTADA
+
+*/
 #include <string>
 #include <iostream>
 using namespace std;
@@ -6,14 +15,44 @@ using namespace std;
 #include <fstream>
 #include <cstring>
 
-vector <unsigned char*> Palabras;
+vector <unsigned char*> VectorPalabras; 
 
-int indice[28];
+int indice[26]; // indice para cada letra del alfabeto
+
+int busqueda_binaria( vector<unsigned char*>& VectorPalabras,unsigned char* palabra,int inicio, int fin){
+   // busqueda binaria para buscar palabra en 
+   while (inicio <= fin){
+      int medio = inicio + (fin - inicio) / 2;
+      //strcmp compara la palabra de medio con la palabra buscada y devuelve un numero,
+      //0 si son iguales, un neg si es menor, y un pos si es mayor.
+      int comparacion= strcmp((char*)VectorPalabras[medio], (char*)palabra);
+      
+      if (comparacion == 0) {
+         return medio;
+      } else if (comparacion < 0) {
+         inicio = medio + 1;
+      } else {
+         fin = medio - 1;
+      }
+   }
+   return -1; // Elemento no encontrado
+}
 
 
-void ordenar_lexico( vector<unsigned char*>& Palabras,unsigned char*, int indice);
+void añadir_vector( vector<unsigned char*>& VectorPalabras,unsigned char* palabra, int* indice){
+   /*
+   esta funcion recibe el vector de palabras, una palabra nueva, y el indice con las posiciones de las letras
+   tiene que insetrar la palabra de manera que se mantenga el orden, y actualizar el indice si es necesario.
+   Si supera el maximo del vector, llamar a funcion para crear la copia mas grande (aun no existe).
+   Vector ya crece automaticamente???
+   */
 
-void cargarDiccionario(const string& nombreArchivo, vector<unsigned char*>& Palabras,int indice) {
+
+
+}
+
+
+void cargarDiccionario(const string& nombreArchivo, vector<unsigned char*>& VectorPalabras,int indice) {
     ifstream archivo(nombreArchivo);
     string linea;
      while (getline(archivo, linea)) {
